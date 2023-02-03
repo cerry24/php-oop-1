0841b3f8c865561647147373288c18cb677204b9
cerry24 <samuelecerretti@gmail.com>
2023-02-03 16:30:22 +0100

    add starWars/piratesCaribbean istance & print them

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <!DOCTYPE html>
+<?php 
+    require_once __DIR__ . '/models/Movie.php';
+
+    $PiratesCaribbean = new Movie('Pirates of the caribbean: the course of the black pearl', 'adventure', '80', ['Johnny Depp', 'Orlando Bloom', 'Keira Knightley', 'Geoffrey Rush', 'Kevin McNally', 'Kevin McNally']);
+    $starWars = new Movie('Star wars: a new hope', 'space opera', '93', ['Mark Hamill', 'Carrie Fisher', 'Harrison Ford', 'Alec Guinness', 'Peter Mayhew', 'David Prowse']);
+?>
+
 <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -7,6 +14,40 @@
         <title>OOP 1</title>
     </head>
     <body>
-        
+        <main>
+            <article>
+                <?php
+                    echo "
+                        <h2>$PiratesCaribbean->title</h2>
+                        <p>Genre: $PiratesCaribbean->genre</p>
+                        <p>Rotten tomatoes: $PiratesCaribbean->rating%</p>";
+
+                        echo '<p>Cast</p>';
+
+                        echo '<ul>';
+                            foreach ($PiratesCaribbean->cast as $member) {
+                                echo "<li>$member</li>";
+                            }
+                        echo '</ul>';
+                ?>
+            </article>
+
+            <article>
+                <?php
+                    echo "
+                        <h2>$starWars->title</h2>
+                        <p>Genre: $starWars->genre</p>
+                        <p>Rotten tomatoes: $starWars->rating%</p>";
+
+                        echo '<p>Cast</p>';
+
+                        echo '<ul>';
+                            foreach ($starWars->cast as $member) {
+                                echo "<li>$member</li>";
+                            }
+                        echo '</ul>';
+                ?>
+            </article>
+        </main>
     </body>
 </html>
